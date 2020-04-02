631b0fa5131711fcc88be3a034e1cf7fc9085fee
Erik Ostlund <erik.ostlund@infinitecampus.com>
2020-04-02 11:33:55 -0500

    Resolver bug fix
    if no local recipes, fetch from server

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -9,13 +9,25 @@ import {
 import { DataStorageService } from '../shared/data-storage.service';
 
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
-	constructor(private dataStorageService: DataStorageService) {}
+	constructor(
+		private dataStorageService: DataStorageService,
+		private recipesService: RecipeService
+	) {}
 
 	// resolve will subscribe for us
 	resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-		return this.dataStorageService.fetchRecipes();
+		// for now 'getRecipes' is getting them locally--not from server!
+		const recipes = this.recipesService.getRecipes();
+
+		// if no local recipes, fetch from server
+		if (recipes.length === 0) {
+			return this.dataStorageService.fetchRecipes();
+		} else {
+			return recipes;
+		}
 	}
 }
